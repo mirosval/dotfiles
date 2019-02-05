500e3a5912e61fc5c512cc3cd71497b6191d95e6
Miroslav Zoricak <miroslav.zoricak@numberfour.eu>
2019-02-05 10:25:47 +0100

    Re-map idea to hyper-e rather than hyper-i

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,7 +2,7 @@ hyper = {"ctrl", "alt", "cmd", "shift"}
 
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind(hyper, "i", function() hs.application.launchOrFocus("IntelliJ IDEA Ultimate") end)
+hs.hotkey.bind(hyper, "e", function() hs.application.launchOrFocus("IntelliJ IDEA Ultimate") end)
 hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Firefox") end)
 hs.hotkey.bind(hyper, "m", function() hs.application.launchOrFocus("Messages") end)
 hs.hotkey.bind(hyper, "g", function() hs.application.launchOrFocus("Fork") end)
