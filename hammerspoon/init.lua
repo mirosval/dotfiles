076be8adb89d255d78eb6121ea93068f439f8447
Miroslav Zoricak <miroslav.zoricak@numberfour.eu>
2019-02-20 14:11:02 +0100

    Add more hammerspoon configs

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,8 +2,10 @@ hyper = {"ctrl", "alt", "cmd", "shift"}
 
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
 hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("iTerm") end)
+hs.hotkey.bind(hyper, "y", function() hs.application.launchOrFocus("GraphiQL") end)
 hs.hotkey.bind(hyper, "e", function() hs.application.launchOrFocus("IntelliJ IDEA Ultimate") end)
 hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Firefox") end)
+hs.hotkey.bind(hyper, "c", function() hs.application.launchOrFocus("Visual Studio Code") end)
 hs.hotkey.bind(hyper, "m", function() hs.application.launchOrFocus("Messages") end)
 hs.hotkey.bind(hyper, "g", function() hs.application.launchOrFocus("Fork") end)
 hs.hotkey.bind(hyper, "s", function() hs.application.launchOrFocus("Slack") end)
