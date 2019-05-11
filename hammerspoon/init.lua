5f7bcc050d9ec354d26aa0d6eb0db246f77e95c5
Miroslav Zoricak <miroslav.zoricak@numberfour.eu>
2019-05-11 12:47:16 +0200

    Rearrange shortcuts to support Dash an TablePlus

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,17 @@
 hyper = {"ctrl", "alt", "cmd", "shift"}
 
 hs.hotkey.bind(hyper, "v", function() hs.eventtap.keyStrokes(hs.pasteboard.getContents()) end)
-hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("iTerm") end)
-hs.hotkey.bind(hyper, "y", function() hs.application.launchOrFocus("GraphiQL") end)
+hs.hotkey.bind(hyper, "w", function() hs.application.launchOrFocus("iTerm") end)
+hs.hotkey.bind(hyper, "q", function() hs.application.launchOrFocus("GraphiQL") end)
 hs.hotkey.bind(hyper, "e", function() hs.application.launchOrFocus("IntelliJ IDEA Ultimate") end)
+hs.hotkey.bind(hyper, "t", function() hs.application.launchOrFocus("TablePlus") end)
 hs.hotkey.bind(hyper, "b", function() hs.application.launchOrFocus("Firefox") end)
 hs.hotkey.bind(hyper, "c", function() hs.application.launchOrFocus("Visual Studio Code") end)
 hs.hotkey.bind(hyper, "m", function() hs.application.launchOrFocus("Messages") end)
 hs.hotkey.bind(hyper, "g", function() hs.application.launchOrFocus("Fork") end)
 hs.hotkey.bind(hyper, "s", function() hs.application.launchOrFocus("Slack") end)
 hs.hotkey.bind(hyper, "d", function() hs.application.launchOrFocus("Spotify") end)
+hs.hotkey.bind(hyper, "a", function() hs.application.launchOrFocus("Dash") end)
 
 hs.loadSpoon("MiroWindowsManager")
 
