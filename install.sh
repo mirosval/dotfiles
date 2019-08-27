23f0795a401f44abf8b9ead718def72b6c155c2b
Miroslav Zoricak <miroslav.zoricak@numberfour.eu>
2019-08-27 16:15:06 +0200

    Use Brewfile instead of a custom script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 DOTFILES=$HOME/.dotfiles
 
+ln -s $DOTFILES/Brewfile $HOME/.Brewfile
 ln -s $DOTFILES/config/alacritty $HOME/.config/alacritty
 ln -s $DOTFILES/config/nvim $HOME/.config/nvim
 ln -s $DOTFILES/config/nvim/init.vim $HOME/.ideavimrc
@@ -11,12 +12,27 @@ ln -s $DOTFILES/ctags.symlink $HOME/.ctags
 ln -s $DOTFILES/git/ignore $HOME/.gitignore
 ln -s $DOTFILES/tmux/tmux.conf.symlink $HOME/.tmux.conf
 
-$DOTFILES/install/brew.sh
 $DOTFILES/install/osx.sh
 $DOTFILES/install/tmux.sh
 
-sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
-chsh -s $(which zsh)
+brew bundle
+
+# After the install setup fzf
+echo -e "\\n\\nRunning fzf install script..."
+echo "=============================="
+/usr/local/opt/fzf/install --all --no-bash --no-fish
+
+# Change the default shell to zsh
+zsh_path="$( command -v zsh )"
+if ! grep "$zsh_path" /etc/shells; then
+    echo "adding $zsh_path to /etc/shells"
+    echo "$zsh_path" | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "$zsh_path" ]]; then
+    chsh -s "$zsh_path"
+    echo "default shell changed to $zsh_path"
+fi
 
 pip3 install neovim sexpdata websocket-client
 
