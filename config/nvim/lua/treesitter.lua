80bbb371ce9644d86e8679701ec7f12296ef86c2
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2021-05-21 15:13:04 +0200

    add treesitter languages

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,4 +1,26 @@
 require'nvim-treesitter.configs'.setup{
+    ensure_installed = {
+        "bash",
+        "css",
+        "dockerfile",
+        "go",
+        "graphql",
+        "html",
+        "javascript",
+        "java",
+        "jsdoc",
+        "json",
+        "julia",
+        "kotlin",
+        "lua",
+        "python",
+        "rust",
+        "scala",
+        "toml",
+        "tsx",
+        "typescript",
+        "yaml"
+    },
     autopairs = {
         enable = true
     },
