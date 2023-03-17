c058b0ffed6f77c391e6b0d75a5df802e368a34b
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-03-17 23:28:22 +0100

    improve tmux

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -2,9 +2,16 @@
 {
   programs.tmux = {
     enable = true;
+    clock24 = true;
+    baseIndex = 1;
+    historyLimit = 50000;
+    keyMode = "vi";
+    mouse = true;
+    # tmuxp.enable = true;
     extraConfig = ''
       set -g default-terminal "tmux-256color"
       set-option -sa terminal-overrides ',alacritty:Tc'
+      set -g pane-base-index 1
     '';
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
@@ -13,12 +20,13 @@
     ];
   };
 
-#  home.packages = with pkgs; [
-#    tmuxp
-#  ];
-#
-#  xdg.configFile = {
-#    "tmuxp/dot.yaml".source = ./tmuxp/dot.yaml;
-#    "tmuxp/pro.yaml".source = ./tmuxp/pro.yaml;
-#  };
+
+  home.packages = with pkgs; [
+    tmuxp
+  ];
+
+  xdg.configFile = {
+    "tmuxp/dot.yaml".source = ./tmuxp/dot.yaml;
+    "tmuxp/pro.yaml".source = ./tmuxp/pro.yaml;
+  };
 }
