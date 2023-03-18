12f5a24e918c30f8b63a16c5fbf88460679bc58e
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-03-18 22:55:21 +0100

    fix session switching

--- modules/tmux/default.nix
+++ modules/tmux/default.nix
@@ -12,6 +12,8 @@
       set -g default-terminal "tmux-256color"
       set-option -sa terminal-overrides ',alacritty:Tc'
       set -g pane-base-index 1
+      # session switching
+      bind s display-popup -E "tmux list-sessions | sed -E 's/:.*$//' | grep -v \"^$(tmux display-message -p '#S')\$\" | fzf --reverse | xargs tmux switch-client -t"
     '';
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
@@ -23,6 +25,7 @@
 
   home.packages = with pkgs; [
     tmuxp
+    fzf
   ];
 
   xdg.configFile = {
