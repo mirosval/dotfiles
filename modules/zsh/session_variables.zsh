f38ab222b3b4efa62730a801c3e4955bef7aa6b6
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-08-22 14:25:39 +0200

    fix missing nix_path issue
    apparently this now has to be set manually https://github.com/NixOS/nixpkgs/issues/149791

--- modules/zsh/session_variables.zsh
+++ modules/zsh/session_variables.zsh
@@ -1,5 +1,6 @@
 export DOTFILES=~/.dotfiles
 export PATH="$HOME/.local/bin:$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:$PATH"
+export NIX_PATH=${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels
 # required by tmuxp
 export DISABLE_AUTO_TITLE='true'
 export RIPGREP_CONFIG_PATH=~/.config/rg/rgrc
