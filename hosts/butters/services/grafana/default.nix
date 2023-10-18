29128f7f550b279d8c51e533dd606e51e4de2b00
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-10-18 13:06:48 +0200

    fix grafana

--- hosts/butters/services/grafana/default.nix
+++ hosts/butters/services/grafana/default.nix
@@ -3,9 +3,10 @@
     enable = true;
     settings = {
       server = {
-        domain = "grafana.lan.zoricak.net";
+        router_logging = true;
         http_port = 2342;
         http_addr = "127.0.0.1";
+        root_url = "http://grafana.lan.zoricak.net/";
       };
     };
   };
@@ -21,9 +22,9 @@
           addr = config.services.grafana.settings.server.http_addr;
           port = toString config.services.grafana.settings.server.http_port;
         in
-        {
+        [{
           url = "http://${addr}:${port}";
-        };
+        }];
     };
   };
 }
