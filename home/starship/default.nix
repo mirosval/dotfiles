d2d383482dd10535922ff253c89f27ae32211e11
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2023-10-25 15:43:50 +0200

    restore lost starship options

--- home/starship/default.nix
+++ home/starship/default.nix
@@ -2,5 +2,14 @@ _: {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      add_newline = false;
+
+      battery = {
+        full_symbol = "🔋";
+        charging_symbol = "⚡️";
+        discharging_symbol = "💀";
+      };
+    };
   };
 }
