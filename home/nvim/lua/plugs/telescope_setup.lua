5ff6098ce6ac1653cf5187eab24573edf1c1c698
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2024-06-18 08:01:07 +0200

    improve telescope on long paths
    improves telescope config when the paths in the project are too long and only the beginning is visible. Now it shows the path truncated such that you can see the file name

--- home/nvim/lua/plugs/telescope_setup.lua
+++ home/nvim/lua/plugs/telescope_setup.lua
@@ -1,21 +1,24 @@
-local telescope = require('telescope')
+local telescope = require("telescope")
 -- local lib = require('lib')
 -- local nmap = lib.nmap
 
-telescope.setup{
+telescope.setup({
   defaults = {
-    file_sorter =  require'telescope.sorters'.get_fzy_sorter,
+    file_sorter = require("telescope.sorters").get_fzy_sorter,
+    path_display = {
+      "truncate",
+    },
   },
   pickers = {
     find_files = {
       -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
       find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-    }
+    },
   },
   extensions = {
-    ["ui-select"] = {}
-  }
-}
+    ["ui-select"] = {},
+  },
+})
 --
 -- nmap("<C-p>", function() require('telescope.builtin').find_files({ hidden = true }) end)
 -- nmap("<Leader>fg", ':Telescope live_grep<CR>')
