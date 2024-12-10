64fa96ab280cac3c188e23b9afd3e334122d1aaa
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2024-12-10 21:43:42 +0100

    immich

--- hosts/butters/services/immich/default.nix
+++ hosts/butters/services/immich/default.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, ... }:
 let
   name = "immich";
-  immichVersion = "v1.121.0";
+  immichVersion = "v1.122.2";
   dbPath = "/var/containers/immich/db";
   dbBackupPath = "/var/containers/immich/backups";
   uploadPath = "/mnt/immich";
