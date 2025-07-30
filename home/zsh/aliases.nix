298a2cf8e4b7aba6815e93d76ec80a9ccb23bfe2
Miroslav Zoricak <miroslav.zoricak@coralogix.com>
2025-07-30 08:50:21 +0200

    add jj aliases

--- home/zsh/aliases.nix
+++ home/zsh/aliases.nix
@@ -1,77 +1,77 @@
 {
-  zshconfig="code ~/.zshrc";
+  zshconfig = "code ~/.zshrc";
 
-  l="eza -lah";
-  lg="eza -lah --git";
+  l = "eza -lah";
+  lg = "eza -lah --git";
 
-  v="nvim . && clear";
+  v = "nvim . && clear";
 
-  c="clear ; tmux clear-history ; clear";
+  c = "clear ; tmux clear-history ; clear";
 
-  j="z";
+  j = "z";
 
-  did="vim +'normal Go' +'r!date' ~/did.txt";
+  did = "vim +'normal Go' +'r!date' ~/did.txt";
 
   # use nvim, but don't make me think about it
-  vim="nvim";
+  vim = "nvim";
 
   # Filesystem aliases
-  ".."="cd ..";
-  "..."="cd ../..";
-  "...."="cd ../../..";
-  "....."="cd ../../../..";
+  ".." = "cd ..";
+  "..." = "cd ../..";
+  "...." = "cd ../../..";
+  "....." = "cd ../../../..";
 
   # IP addresses
-  my_ip="dig +short myip.opendns.com @resolver1.opendns.com";
-  localip="ipconfig getifaddr en1";
-  ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'";
+  my_ip = "dig +short myip.opendns.com @resolver1.opendns.com";
+  localip = "ipconfig getifaddr en1";
+  ips = "ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'";
 
   # Flush Directory Service cache
-  flush="sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder";
+  flush = "sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder";
 
   # Hide/show all desktop icons (useful when presenting)
-  hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder";
-  showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder";
-
+  hidedesktop = "defaults write com.apple.finder CreateDesktop -bool false && killall Finder";
+  showdesktop = "defaults write com.apple.finder CreateDesktop -bool true && killall Finder";
 
   # Brew
-  buc="brew upgrade && brew cleanup";
+  buc = "brew upgrade && brew cleanup";
 
   # Cargo
-  cb="cargo build";
-  ct="cargo test";
-  cr="cargo run";
-  ctn="cargo test -- --nocapture";
-  cwc="cargo watch -c -x 'check'";
-  cwct="cargo watch -c -x 'check' -x 'test -- --nocapture'";
-  cwctr="cargo watch -c -x 'check' -x 'test -- --nocapture' -x 'run'";
+  cb = "cargo build";
+  ct = "cargo test";
+  cr = "cargo run";
+  ctn = "cargo test -- --nocapture";
+  cwc = "cargo watch -c -x 'check'";
+  cwct = "cargo watch -c -x 'check' -x 'test -- --nocapture'";
+  cwctr = "cargo watch -c -x 'check' -x 'test -- --nocapture' -x 'run'";
 
   # Git
-  gpl="git pull";
-  gps="git push";
-  gpr="git push && gh pr create --fill --web";
-  gb="git checkout -b";
-  ghv="gh repo view --web";
-  gm="git checkout master || git checkout main";
-  gst="git status";
+  gpl = "git pull";
+  gps = "git push";
+  gpr = "git push && gh pr create --fill --web";
+  gb = "git checkout -b";
+  ghv = "gh repo view --web";
+  gm = "git checkout master || git checkout main";
+  gst = "git status";
+
+  # JJ
+  jr = "jj rebase -d master";
+  jp = "jj log --no-pager";
 
   # tmuxp
-  tl="tmuxp load --yes";
+  tl = "tmuxp load --yes";
 
   # Restarts coreaudio, use when headphones don't work
-  fix_audio="sudo pkill coreaudiod";
+  fix_audio = "sudo pkill coreaudiod";
 
   # Docker-compose alias
-  dc="docker-compose";
-  dcu="docker-compose up";
-  dcd="docker-compose down";
+  dc = "docker-compose";
+  dcu = "docker-compose up";
+  dcd = "docker-compose down";
 
   # K8s
-  k8="kubectl";
-  km="kubectl -n master";
+  k8 = "kubectl";
+  km = "kubectl -n master";
 
-  news="w3m https://news.ycombinator.com";
-
-  # Julia
-  jp="julia --project=.";
+  news = "w3m https://news.ycombinator.com";
 }
