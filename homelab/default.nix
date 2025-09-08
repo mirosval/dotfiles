e73bd7817836a1dc81a30d042a43d73ad9a5cb3a
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2025-09-08 22:06:45 +0200

    add argo

--- homelab/default.nix
+++ homelab/default.nix
@@ -2,4 +2,7 @@
   nixidy.target.repository = "https://github.com/mirosval/dotfiles.git";
   nixidy.target.branch = "master";
   nixidy.target.rootPath = "./target/manifests/homelab";
+  imports = [
+    ./argocd.nix
+  ];
 }
