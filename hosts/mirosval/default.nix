276e4cde34f6140c6f73fd2e6b150ea579cabcc5
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2026-03-16 19:43:59 +0100

    add homelab

--- hosts/mirosval/default.nix
+++ hosts/mirosval/default.nix
@@ -3,33 +3,33 @@ _: {
     ../../modules/darwin
   ];
 
-  homebrew = {
-    enable = true;
-    brews = [ ];
-    casks = [
-      "discord"
-      "docker"
-      "firefox"
-      "flux"
-      "fork"
-      "google-chrome"
-      "hammerspoon"
-      "handbrake"
-      "iina"
-      "logseq"
-      "quicklook-csv"
-      "quicklook-json"
-      "raycast"
-      "spotify"
-      "steam"
-      "tableplus"
-      "tailscale"
-      "the-unarchiver"
-      "tunnelblick"
-      "visual-studio-code"
-      "vlc"
-    ];
-  };
+  # homebrew = {
+  #   enable = true;
+  #   brews = [ ];
+  #   casks = [
+  #     "discord"
+  #     "docker"
+  #     "firefox"
+  #     "flux"
+  #     "fork"
+  #     "google-chrome"
+  #     "hammerspoon"
+  #     "handbrake"
+  #     "iina"
+  #     "logseq"
+  #     "quicklook-csv"
+  #     "quicklook-json"
+  #     "raycast"
+  #     "spotify"
+  #     "steam"
+  #     "tableplus"
+  #     "tailscale"
+  #     "the-unarchiver"
+  #     "tunnelblick"
+  #     "visual-studio-code"
+  #     "vlc"
+  #   ];
+  # };
 
   networking = {
     computerName = "Miro Home MBP";
