10010124b1a6f142f7f61aad97a61ee09bb991bd
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2026-07-03 21:48:27 +0200

    reformat

--- modules/hosts/stan/configuration.nix
+++ modules/hosts/stan/configuration.nix
@@ -1,143 +1,143 @@
-# # Edit this configuration file to define what should be installed on
-# # your system.  Help is available in the configuration.nix(5) man page
-# # and in the NixOS manual (accessible by running ‘nixos-help’).
-#
-# { config, pkgs, ... }:
-
 { self, inputs, ... }: {
-  flake.nixosModules.stanConfiguration = {pkgs, lib, ...}: {
+  flake.nixosModules.stanConfiguration = { pkgs, lib, ... }: {
 
-	  imports =
-	    [ # Include the results of the hardware scan.
-	      self.nixosModules.stanHardware
-	    ];
+    imports = [
+      # Include the results of the hardware scan.
+      self.nixosModules.stanHardware
+    ];
 
-	  # Bootloader.
-	  boot.loader.systemd-boot.enable = true;
-	  boot.loader.efi.canTouchEfiVariables = true;
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-	  # Use latest kernel.
-	  boot.kernelPackages = pkgs.linuxPackages_latest;
+    # Use latest kernel.
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
-	  nix.settings.experimental-features = ["nix-command" "flakes"];
+    nix.settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
 
-	  networking.hostName = "stan"; # Define your hostname.
-	  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    networking.hostName = "stan"; # Define your hostname.
+    # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-	  # Configure network proxy if necessary
-	  # networking.proxy.default = "http://user:password@proxy:port/";
-	  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-	  # Enable networking
-	  networking.networkmanager.enable = true;
+    # Enable networking
+    networking.networkmanager.enable = true;
 
-	  # Set your time zone.
-	  time.timeZone = "Europe/Berlin";
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
 
-	  # Select internationalisation properties.
-	  i18n.defaultLocale = "en_US.UTF-8";
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
 
-	  i18n.extraLocaleSettings = {
-	    LC_ADDRESS = "de_DE.UTF-8";
-	    LC_IDENTIFICATION = "de_DE.UTF-8";
-	    LC_MEASUREMENT = "de_DE.UTF-8";
-	    LC_MONETARY = "de_DE.UTF-8";
-	    LC_NAME = "de_DE.UTF-8";
-	    LC_NUMERIC = "de_DE.UTF-8";
-	    LC_PAPER = "de_DE.UTF-8";
-	    LC_TELEPHONE = "de_DE.UTF-8";
-	    LC_TIME = "de_DE.UTF-8";
-	  };
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
 
-	  # Enable the X11 windowing system.
-	  # You can disable this if you're only using the Wayland session.
-	  services.xserver.enable = true;
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    services.xserver.enable = true;
 
-	  # Enable the KDE Plasma Desktop Environment.
-	  services.displayManager.sddm.enable = true;
-	  services.desktopManager.plasma6.enable = true;
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
 
-	  # Configure keymap in X11
-	  services.xserver.xkb = {
-	    layout = "us";
-	    variant = "";
-	  };
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "us";
+      variant = "";
+    };
 
-	  # Enable CUPS to print documents.
-	  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
 
-	  # Enable sound with pipewire.
-	  services.pulseaudio.enable = false;
-	  security.rtkit.enable = true;
-	  services.pipewire = {
-	    enable = true;
-	    alsa.enable = true;
-	    alsa.support32Bit = true;
-	    pulse.enable = true;
-	    # If you want to use JACK applications, uncomment this
-	    #jack.enable = true;
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-	    # use the example session manager (no others are packaged yet so this is enabled by default,
-	    # no need to redefine it in your config for now)
-	    #media-session.enable = true;
-	  };
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-	  # Enable touchpad support (enabled default in most desktopManager).
-	  # services.xserver.libinput.enable = true;
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
 
-	  # Define a user account. Don't forget to set a password with ‘passwd’.
-	  users.users."miro" = {
-	    isNormalUser = true;
-	    description = "miro";
-	    extraGroups = [ "networkmanager" "wheel" ];
-	    packages = with pkgs; [
-	      kdePackages.kate
-	    #  thunderbird
-	    ];
-	  };
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users."miro" = {
+      isNormalUser = true;
+      description = "miro";
+      extraGroups = [
+        "networkmanager"
+        "wheel"
+      ];
+      packages = with pkgs; [
+        kdePackages.kate
+        #  thunderbird
+      ];
+    };
 
-	  # Install firefox.
-	  programs.firefox.enable = true;
+    # Install firefox.
+    programs.firefox.enable = true;
 
-	  # Allow unfree packages
-	  nixpkgs.config.allowUnfree = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 
-	  # List packages installed in system profile. To search, run:
-	  # $ nix search wget
-	  environment.systemPackages = with pkgs; [
-	  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	  #  wget
-	  neovim
-	  git
-	  gh
-	  ];
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+      neovim
+      git
+      gh
+    ];
 
-	  # Some programs need SUID wrappers, can be configured further or are
-	  # started in user sessions.
-	  # programs.mtr.enable = true;
-	  # programs.gnupg.agent = {
-	  #   enable = true;
-	  #   enableSSHSupport = true;
-	  # };
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-	  # List services that you want to enable:
+    # List services that you want to enable:
 
-	  # Enable the OpenSSH daemon.
-	  # services.openssh.enable = true;
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
-	  # Open ports in the firewall.
-	  # networking.firewall.allowedTCPPorts = [ ... ];
-	  # networking.firewall.allowedUDPPorts = [ ... ];
-	  # Or disable the firewall altogether.
-	  # networking.firewall.enable = false;
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
 
-	  # This value determines the NixOS release from which the default
-	  # settings for stateful data, like file locations and database versions
-	  # on your system were taken. It‘s perfectly fine and recommended to leave
-	  # this value at the release version of the first install of this system.
-	  # Before changing this value read the documentation for this option
-	  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	  system.stateVersion = "26.05"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "26.05"; # Did you read the comment?
 
-	};
+  };
 }
