960c297890091b46bc1b283a0631f7473e2d833f
Miroslav Zoricak <1315417+mirosval@users.noreply.github.com>
2026-07-04 23:31:18 +0200

    niri keybinds

--- modules/nixos/niri.nix
+++ modules/nixos/niri.nix
@@ -28,26 +28,26 @@
           binds = {
             "Mod+Return".spawn = config.terminal;
 
-            "Mod+Q".close-window = null;
-            "Mod+F".maximize-column = null;
-            "Mod+G".fullscreen-window = null;
-            "Mod+Shift+F".toggle-window-floating = null;
-            "Mod+C".center-column = null;
+            "Mod+Q".close-window = _: { };
+            "Mod+F".maximize-column = _: { };
+            "Mod+G".fullscreen-window = _: { };
+            "Mod+Shift+F".toggle-window-floating = _: { };
+            "Mod+C".center-column = _: { };
 
-            "Mod+H".focus-column-left = null;
-            "Mod+L".focus-column-right = null;
-            "Mod+K".focus-window-up = null;
-            "Mod+J".focus-window-down = null;
+            "Mod+H".focus-column-left = _: { };
+            "Mod+L".focus-column-right = _: { };
+            "Mod+K".focus-window-up = _: { };
+            "Mod+J".focus-window-down = _: { };
 
-            "Mod+Left".focus-column-left = null;
-            "Mod+Right".focus-column-right = null;
-            "Mod+Up".focus-window-up = null;
-            "Mod+Down".focus-window-down = null;
+            "Mod+Left".focus-column-left = _: { };
+            "Mod+Right".focus-column-right = _: { };
+            "Mod+Up".focus-window-up = _: { };
+            "Mod+Down".focus-window-down = _: { };
 
-            "Mod+Shift+H".move-column-left = null;
-            "Mod+Shift+L".move-column-right = null;
-            "Mod+Shift+K".move-window-up = null;
-            "Mod+Shift+J".move-window-down = null;
+            "Mod+Shift+H".move-column-left = _: { };
+            "Mod+Shift+L".move-column-right = _: { };
+            "Mod+Shift+K".move-window-up = _: { };
+            "Mod+Shift+J".move-window-down = _: { };
 
             "Mod+1".focus-workspace = "w0";
             "Mod+2".focus-workspace = "w1";
@@ -82,10 +82,10 @@
             "Mod+Ctrl+J".set-window-height = "-5%";
             "Mod+Ctrl+K".set-window-height = "+5%";
 
-            "Mod+WheelScrollDown".focus-column-left = null;
-            "Mod+WheelScrollUp".focus-column-right = null;
-            "Mod+Ctrl+WheelScrollDown".focus-workspace-down = null;
-            "Mod+Ctrl+WheelScrollUp".focus-workspace-up = null;
+            "Mod+WheelScrollDown".focus-column-left = _: { };
+            "Mod+WheelScrollUp".focus-column-right = _: { };
+            "Mod+Ctrl+WheelScrollDown".focus-workspace-down = _: { };
+            "Mod+Ctrl+WheelScrollUp".focus-workspace-up = _: { };
 
             "Mod+Ctrl+S".spawn-sh =
               "${lib.getExe config.pkgs.grim} -l 0 - | ${config.pkgs.wl-clipboard}/bin/wl-copy";
